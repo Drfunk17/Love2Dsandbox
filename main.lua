f48eb60f91870bf2c17bc455b5aad1437af62f91
Drfunk17 <Tyson.Smith17@outlook.com>
2024-08-15 21:38:40 -0400

    Day4

--- main.lua
+++ main.lua
@@ -13,3 +13,41 @@ function love.errorhandler(msg)
 end
 
 print("Hello World")
+
+function love.load()
+    rectangleXPos = 100
+    rectangleYPos = 50
+    circleXPos = 300
+    circleYPos = 300
+    moveSpeed = 100
+    circlemovespeed = 200
+end
+
+function love.update(deltaTime)
+    -- print(deltaTime)
+
+    if love.keyboard.isDown("right") then
+        rectangleXPos = rectangleXPos + moveSpeed * deltaTime
+    elseif love.keyboard.isDown("left") then
+        rectangleXPos = rectangleXPos - moveSpeed * deltaTime
+    end
+
+    if love.keyboard.isDown("up") then
+        rectangleYPos = rectangleYPos - moveSpeed * deltaTime
+    elseif love.keyboard.isDown("down") then
+        rectangleYPos = rectangleYPos + moveSpeed * deltaTime
+    end
+
+
+
+    if love.keyboard.isDown("w") then
+        circleYPos = circleYPos - circlemovespeed * deltaTime
+    elseif love.keyboard.isDown("s") then
+        circleYPos = circleYPos + circlemovespeed * deltaTime
+    end
+end
+
+function love.draw()
+    love.graphics.rectangle("line", rectangleXPos, rectangleYPos, 200, 150)
+    love.graphics.circle("line", circleXPos, circleYPos, 50)
+end
